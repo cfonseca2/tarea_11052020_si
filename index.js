e0ac6be6bb4bf4b047c393c48102590682a2d6c0
cfonseca2 <cfonseca2@outlook.com>
2020-05-10 23:30:27 -0500

    todo yagit add .git add .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.send('Mi primer server corriendo en http://localhost:3000"')
+  console.log('Mi primer server corriendo en http://localhost:3000');
 })
 
 app.listen(3000)
